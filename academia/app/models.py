24207263de7c813f527835ffb93466a281e6c43f
Renan Carraro <carrarorenan0@gmail.com>
2025-05-30 21:19:28 -0300

    Add files via upload

--- academia/app/models.py
+++ academia/app/models.py
@@ -13,6 +13,7 @@ class Produto(models.Model):
     )
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     disponivel = models.BooleanField(default=True)
+    descricao = models.CharField(max_length=500, null=True, blank=True)
 
     def __str__(self):
         return self.nome
